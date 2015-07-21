6198b767824eac4c5400a559c7e5a78fbdaa0023
lsb1985 <466255983@qq.com>
2015-07-21 20:15:55 +0800

    at office:init models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8 -*-
+
+from app import db
+
+#使用db.Column来创建字段
+class User(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	nickname=db.Column(db.String(64),index=True,unique=True)
+	email = db.Column(db.String(120), index = True, unique = True)
+
+	#告诉Python如何打印这个类的对象,我们将用它来调试
+	def __repr__(self):
+		return '<User %r>' %(self.nickname)
+
+		
