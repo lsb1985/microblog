3dd9b61a4eb4fc8426fefcf980f51bf2f157b596
lsb1985 <466255983@qq.com>
2015-07-24 14:42:55 +0800

    at office:change config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ SECRET_KEY='you-will-never-guess'
 
 # OpenID 提供者的列表
 OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Google', 'url': 'https://accounts.google.com/ServiceLogin'},
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
